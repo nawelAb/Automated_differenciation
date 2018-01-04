1ef5a58f5b407b0e74f2fd7c5b8ece409a4fa04a
naoual abbou <naoual.abbou@gmail.com>
2018-01-03 18:54:07 -0800

    test

--- libraries/diffTree.cpp
+++ libraries/diffTree.cpp
@@ -56,9 +56,45 @@ namespace ourlibs {
 {
 }
 
-  values* resolution()
-{
-}
+    values* resolution() {   // arbre de résolution recursive 
+   
+  	if(diff[0] != nullptr) 
+      diff[0].resolution(); 
+
+    if(diff[1] != nullptr) 
+      diff[1].resolution();
+
+ 
+    swicth (ourOperator) : 
+    case ("pow") : {
+      // l entier de la classe diffTree est considéré 
+      //comme une puissance 
+      values tmp = values(m_tabdiv[0].size()) ; // cree un objet tmp 
+     // power  *  x puissance (power - 1)
+      tmp.values = m_tabdiv[0];
+    // vérif du contenu de values pour voire si 
+    //il faut faire appel au opérations values
+     
+
+    break;
+    } 
+
+    case ("mul") : {
+      values tmp = values(m_tabdiv[1].size()) ;  
+      tmp.values = m_tabdiv[1];
+
+    break;
+    } 
+
+    case ("add") : {
+      values tmp = values(m_tabdiv[1].size()) ;  
+      tmp.m_tabdiv = m_tabdiv[1]; // result 
+
+    break;
+    } 
+  }
+
+
 }
 
 
